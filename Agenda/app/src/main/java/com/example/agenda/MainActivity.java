155578e1cabac6de35d16d4b17ff2b858f942c3b
Thiago Aires da Silva <makarrao@gmail.com>
2019-06-05 11:43:12 -0300

    Android / Modulo 1 / Inicial com toast

--- Agenda/app/src/main/java/com/example/agenda/MainActivity.java
+++ Agenda/app/src/main/java/com/example/agenda/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.agenda;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-
+        Toast.makeText(this, "Bom dia!", Toast.LENGTH_SHORT).show();
+    }
 }
