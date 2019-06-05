706a3dd803c2464dba8161ea7d5a94041976a8b2
Thiago Aires da Silva <makarrao@gmail.com>
2019-06-05 12:28:33 -0300

    Android / Modulo 1 / definindo valor de TextView a partir de um array

--- Agenda/app/src/main/java/com/example/agenda/MainActivity.java
+++ Agenda/app/src/main/java/com/example/agenda/MainActivity.java
@@ -3,13 +3,22 @@ package com.example.agenda;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Bom dia!", Toast.LENGTH_SHORT).show();
+        List<String> listStudents = new ArrayList<>(Arrays.asList("Thiago", "Aires", "Silva"));
+
+        TextView primeiroAluno = findViewById(R.id.textView);
+        primeiroAluno.setText(listStudents.get(1));
     }
 }
