afe0ceb90d1b3f8dbd95a02ed984b58be8eb745a
Thiago Aires <makarrao@gmail.com>
2019-06-23 18:08:13 -0300

    Android completo
    Frases: APP completo

--- AndroidCompleto/Frases/app/src/main/java/com/thiagoas/frases/MainActivity.java
+++ AndroidCompleto/Frases/app/src/main/java/com/thiagoas/frases/MainActivity.java
@@ -3,6 +3,10 @@ package com.thiagoas.frases;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void generateNewPhrase(View view){
+        String[] phrases = {
+                "Tarde",
+                "Noite",
+                "Uai so",
+                "gimme fuel",
+                "oloco bisho"
+        };
+
+        int numberPhrases = new Random().nextInt(phrases.length);
+        TextView textPhrase = findViewById(R.id.tvPhraseGenerated);
+
+        textPhrase.setText(phrases[numberPhrases]);
     }
 }
