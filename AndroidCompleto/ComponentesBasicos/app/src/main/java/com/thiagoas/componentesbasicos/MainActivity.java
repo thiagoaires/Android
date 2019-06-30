409a323504b1843bab158afa9b23609a62fa1edb
Thiago Aires <makarrao@gmail.com>
2019-06-29 23:06:33 -0300

    Android - Componentes basicos

---
+++ AndroidCompleto/ComponentesBasicos/app/src/main/java/com/thiagoas/componentesbasicos/MainActivity.java
@@ -0,0 +1,13 @@
+package com.thiagoas.componentesbasicos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
