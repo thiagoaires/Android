e9537c5cf4f6fed314481a2f00330885f69d7945
Thiago Aires <makarrao@gmail.com>
2019-07-03 08:54:28 -0300

    RecyclerView

--- AndroidCompleto/ReciclerView/app/src/main/java/com/thiagoas/reciclerview/activity/activity/MainActivity.java
+++ AndroidCompleto/ReciclerView/app/src/main/java/com/thiagoas/reciclerview/activity/activity/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 
 import com.thiagoas.reciclerview.R;
+import com.thiagoas.reciclerview.activity.adapter.Adapter;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerViewPadrao;
@@ -18,12 +19,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //adapter
-
+        Adapter adapter = new Adapter();
         //recyclerView
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerViewPadrao.setLayoutManager(layoutManager);
         recyclerViewPadrao.setHasFixedSize(true);
+        recyclerViewPadrao.setAdapter(adapter);
 
 
     }
--- AndroidCompleto/ReciclerView/app/src/main/java/com/thiagoas/reciclerview/activity/adapter/Adapter.java
+++ AndroidCompleto/ReciclerView/app/src/main/java/com/thiagoas/reciclerview/activity/adapter/Adapter.java
@@ -1,5 +1,6 @@
 package com.thiagoas.reciclerview.activity.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -7,22 +8,28 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.thiagoas.reciclerview.R;
+
 public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_lista, parent, false);
+        return new MyViewHolder(itemLista);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        holder.titulo.setText("Title teste");
+        holder.ano.setText("2101");
+        holder.genero.setText("humor");
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 5;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
@@ -33,6 +40,10 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            titulo = itemView.findViewById(R.id.tvTitulo);
+            ano = itemView.findViewById(R.id.tvAno);
+            genero = itemView.findViewById(R.id.tvGenero);
+
         }
     }
 
