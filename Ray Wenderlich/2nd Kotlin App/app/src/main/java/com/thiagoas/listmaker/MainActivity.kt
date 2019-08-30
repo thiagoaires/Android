73f4c6bdfdeacd0e6c7ad5f8059330c4abca5d57
Thiago Aires <makarrao@gmail.com>
2019-08-30 01:58:38 -0300

    2nd Kotlin app / RecyclerView(adapter + viewHolder[populados])

--- Ray Wenderlich/2nd Kotlin App/app/src/main/java/com/thiagoas/listmaker/MainActivity.kt
+++ Ray Wenderlich/2nd Kotlin App/app/src/main/java/com/thiagoas/listmaker/MainActivity.kt
@@ -13,41 +13,17 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-
     lateinit var listRecyclerView: RecyclerView
+
+    var listTitles = arrayOf("tuto tooo", "tutti aoow", "androidTutorials")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
 
         listRecyclerView = findViewById(R.id.lists_recyclerview)
         listRecyclerView.layoutManager = LinearLayoutManager(this)
-        listRecyclerView.adapter = ListSelectionRecyclerViewAdapter()
+        listRecyclerView.adapter = ListSelectionRecyclerViewAdapter(listTitles)
 
-
-
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 }
--- Ray Wenderlich/2nd Kotlin App/app/src/main/java/com/thiagoas/listmaker/adapter/ListSelectionRecyclerViewAdapter.kt
+++ Ray Wenderlich/2nd Kotlin App/app/src/main/java/com/thiagoas/listmaker/adapter/ListSelectionRecyclerViewAdapter.kt
@@ -1,17 +1,28 @@
 package com.thiagoas.listmaker.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.thiagoas.listmaker.R
 
-class ListSelectionRecyclerViewAdapter : RecyclerView.Adapter<ListSelectionViewHolder>(){
+class ListSelectionRecyclerViewAdapter(listTitles: Array<String>) : RecyclerView.Adapter<ListSelectionRecyclerViewAdapter.ListSelectionViewHolder>(){
 
-    var listTitles = arrayOf("tuto tooo", "tutti aoow", "androidTutorials")
+    var listTitles: Array<String>
+
+    init {
+        this.listTitles = listTitles
+    }
+
+    override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
+        holder.listPosition.text = (position + 1).toString()
+        holder.listTitle.text = listTitles.get(position)
+    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
-        val view = LayoutInflater.from(parent?.context).inflate(R.layout.list_selection_view_holder, parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_selection_view_holder, parent,false)
         return ListSelectionViewHolder(view)
     }
 
@@ -19,8 +30,10 @@ class ListSelectionRecyclerViewAdapter : RecyclerView.Adapter<ListSelectionViewH
        return listTitles.size
     }
 
-    override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
-        holder.listPosition.text = (position + 1).toString()
-        holder.listTitle.text = listTitles.get(position)
+    inner class ListSelectionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        val listPosition: TextView = itemView.findViewById(R.id.tvItemNumber)
+        val listTitle: TextView = itemView.findViewById(R.id.tvItemString)
+
     }
 }
